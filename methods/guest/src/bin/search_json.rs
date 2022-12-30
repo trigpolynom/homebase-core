bf35f54809064b41ad4d76e356dbe6d5ba95b4e0
trigpolynom <trigpolynom@gmail.com>
2022-12-30 16:38:17 -0500

    added json

---
+++ methods/guest/src/bin/search_json.rs
@@ -0,0 +1,28 @@
+#![no_main]
+
+use json::parse;
+use json_core::Outputs;
+use risc0_zkvm_guest::{env, sha};
+
+risc0_zkvm_guest::entry!(main);
+
+pub fn main() {
+    let data: String = env::read();
+    let data2: String = env::read();
+
+    let sha = sha::digest(&data.as_bytes());
+    let sha2 = sha::digest(&data2.as_bytes());
+
+
+    let data = parse(&data).unwrap();
+    let data2 = parse(&data2).unwrap();
+
+    if data["critical_data"].as_u32().unwrap() != data2["critical_data"].as_u32() {
+        panic!();
+    }
+    let out = Outputs {
+        hash: *sha,
+        hash2: *sha2,
+    };
+    env::commit(&out);
+}
