ab263b97b2a65ea8ba2cd0f4c71632d5351f1601
trigpolynom <trigpolynom@gmail.com>
2023-02-19 18:22:37 -0500

    using new fhir library

--- core/tests/test.rs
+++ core/tests/test.rs
@@ -6,18 +6,35 @@
 
 use std::fs::File;
 use std::io::prelude::*;
-use homebase_core::Claim;
+// use homebase_core::Claim;
+use fhir::r4::core::{
+    claim::Claim,
+    coverage::Coverage,
+    patient::Patient,
+};
 
 #[test]
 fn parse_claim() -> std::io::Result<()> {
 
-    let mut file = File::open("res/provider_resources/claim.json")?;
+    // let mut file = File::open("res/provider_resources/claim.json")?;
 
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
+    // let mut contents = String::new();
+    // file.read_to_string(&mut contents)?;
 
-    let claim: Claim = serde_json::from_str(&contents)?;
-    println!("{:?}", claim);
+    // let claim: Claim = serde_json::from_str(&contents)?;
+    // println!("{:?}", claim);
+
+    // Load the claim, patient, and coverage resources from some data source
+    let claim = Claim::default();
+    let patient = Patient::default();
+    let coverage = Coverage::default();
+
+    // Create a ClaimPolicy instance and auto-adjudicate the claim
+    let policy = ClaimPolicy {};
+    let eob = policy.auto_adjudicate(&claim, &patient, &coverage);
+
+    // Do something with the EOB, such as write it to a file or send it to a downstream system
+    println!("Auto-adjudication complete. EOB: {:?}", eob);
 
     Ok(())
 
