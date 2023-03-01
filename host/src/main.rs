b69cdd301056cf0fc0d2bdb094fc5d3745bb0cd6
trigpolynom <trigpolynom@gmail.com>
2023-02-28 20:47:01 -0500

    now receiving the error 'code should be provable: eqz failed at: cirgen/components/u32.cpp:266'

--- host/src/main.rs
+++ host/src/main.rs
@@ -2,6 +2,7 @@ use std::{io::prelude::*, fs::File, stringify};
 use homebase_core::{Claim, Patient, Coverage};
 use methods::{VALIDATE_CLAIM_ELF, VALIDATE_CLAIM_ID};
 use risc0_zkvm::{Prover, serde::to_vec};
+
 fn main() {
 
         let mut file = File::open("res/provider_resources/claim.json").unwrap();
@@ -17,23 +18,23 @@ fn main() {
         let mut coverage_contents = String::new();
         file3.read_to_string(&mut coverage_contents).unwrap();
     
-        let claim: Result<Claim, serde_json::Error> = serde_json::from_str(&claim_contents);
-        match claim {
-            Ok(ref c) => println!("{:?}", c),
-            Err(ref e) => eprintln!("Error deserializing claim: {}", e),
-        }
+        // let claim: Result<Claim, serde_json::Error> = serde_json::from_str(&claim_contents);
+        // match claim {
+        //     Ok(ref c) => println!("{:?}", c),
+        //     Err(ref e) => eprintln!("Error deserializing claim: {}", e),
+        // }
     
-        let patient: Result<Patient, serde_json::Error> = serde_json::from_str(&patient_contents);
-        match patient {
-            Ok(ref p) => println!("{:?}", p),
-            Err(ref e) => eprintln!("Error deserializing patient: {}", e),
-        }
+        // let patient: Result<Patient, serde_json::Error> = serde_json::from_str(&patient_contents);
+        // match patient {
+        //     Ok(ref p) => println!("{:?}", p),
+        //     Err(ref e) => eprintln!("Error deserializing patient: {}", e),
+        // }
     
-        let coverage: Result<Coverage, serde_json::Error> = serde_json::from_str(&coverage_contents);
-        match coverage {
-            Ok(ref c) => println!("{:?}", c),
-            Err(ref e) => eprintln!("Error deserializing coverage: {}", e),
-        }
+        // let coverage: Result<Coverage, serde_json::Error> = serde_json::from_str(&coverage_contents);
+        // match coverage {
+        //     Ok(ref c) => println!("{:?}", c),
+        //     Err(ref e) => eprintln!("Error deserializing coverage: {}", e),
+        // }
 
     let mut prover = Prover::new(VALIDATE_CLAIM_ELF, VALIDATE_CLAIM_ID).unwrap();
 
@@ -46,13 +47,13 @@ fn main() {
     // let coverage_bytes = serde_json::to_vec(&coverage.unwrap()).expect("Error serializing coverage");
     // prover.add_input_u8_slice(&coverage_bytes);
 
-    let claim_to_add = &claim.unwrap();
-    let patient_to_add = &patient.unwrap();
-    let coverage_to_add = &coverage.unwrap();
+    // let claim_to_add = &claim.unwrap();
+    // let patient_to_add = &patient.unwrap();
+    // let coverage_to_add = &coverage.unwrap();
 
-    prover.add_input_u32_slice(&to_vec(claim_to_add).expect("Error serializing claim"));
-    prover.add_input_u32_slice(&to_vec(patient_to_add).expect("Error serializing patient"));
-    prover.add_input_u32_slice(&to_vec(coverage_to_add).expect("Error serializing coverage"));
+    prover.add_input_u32_slice(&to_vec(&claim_contents).expect("Error serializing claim"));
+    prover.add_input_u32_slice(&to_vec(&patient_contents).expect("Error serializing patient"));
+    prover.add_input_u32_slice(&to_vec(&coverage_contents).expect("Error serializing coverage"));
 
     let receipt = prover.run().expect("Code should be provable");
 
--- methods/guest/src/bin/validate_claim.rs
+++ methods/guest/src/bin/validate_claim.rs
@@ -7,18 +7,37 @@ use homebase_core::{Claim, Patient, Coverage};
 risc0_zkvm::guest::entry!(main);
 
 pub fn main() {
+
     let mut CORRECT_COVERAGE: bool = false;
 
-    let claim: Claim = env::read();
-    let patient: Patient = env::read();
-    let coverage: Coverage = env::read();
+    let claim_contents: String = env::read();
+    let patient_contents: String = env::read();
+    let coverage_contents: String = env::read();
+
+    let claim: Result<Claim, serde_json::Error> = serde_json::from_str(&claim_contents);
+        match claim {
+            Ok(ref c) => println!("{:?}", c),
+            Err(ref e) => eprintln!("Error deserializing claim: {}", e),
+        }
+    
+        let patient: Result<Patient, serde_json::Error> = serde_json::from_str(&patient_contents);
+        match patient {
+            Ok(ref p) => println!("{:?}", p),
+            Err(ref e) => eprintln!("Error deserializing patient: {}", e),
+        }
+    
+        let coverage: Result<Coverage, serde_json::Error> = serde_json::from_str(&coverage_contents);
+        match coverage {
+            Ok(ref c) => println!("{:?}", c),
+            Err(ref e) => eprintln!("Error deserializing coverage: {}", e),
+        }
 
 
-    let claimInsuranceVec = &claim.insurance.ok_or("some error");
+    let claimInsuranceVec = &claim.unwrap().insurance.ok_or("some error");
 
     let claimInsurance = &claimInsuranceVec.as_ref().unwrap()[0];
 
-    let patientInsurance = &coverage.id;
+    let patientInsurance = &coverage.unwrap().id;
 
     let binding = claimInsurance.coverage.reference.as_ref().unwrap();
     let split = binding.split('/');
@@ -39,7 +58,7 @@ pub fn main() {
     //     panic!("Claim is invalid! You won't be getting reimbursed!");
     // }
 
-    // env::commit(claim);
+    env::commit(&CORRECT_COVERAGE);
 
 }
 
